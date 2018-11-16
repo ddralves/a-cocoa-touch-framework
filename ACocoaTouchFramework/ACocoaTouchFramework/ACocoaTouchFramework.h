1140813c24afb0b8c9dd72713a7ae262af271d91
Dino Alves <ddralves@users.noreply.github.com>
2018-11-16 14:36:50 +0200

    #1 [xcode] Initial setup

---
+++ ACocoaTouchFramework/ACocoaTouchFramework/ACocoaTouchFramework.h
@@ -0,0 +1,18 @@
+//
+//  ACocoaTouchFramework.h
+//  ACocoaTouchFramework
+//
+//  Copyright © 2018 A Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACocoaTouchFramework.
+FOUNDATION_EXPORT double ACocoaTouchFrameworkVersionNumber;
+
+//! Project version string for ACocoaTouchFramework.
+FOUNDATION_EXPORT const unsigned char ACocoaTouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACocoaTouchFramework/PublicHeader.h>
+
+
